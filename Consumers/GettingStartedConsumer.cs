1485ecfc5731250731e1c8581e8c6863ab0fd7a1
Issac-Lopez <lopezissaca@gmail.com>
2024-06-07 10:18:07 -0600

    initial commit

---
+++ Consumers/GettingStartedConsumer.cs
@@ -0,0 +1,25 @@
+namespace GettingStartedConsumer
+{
+
+    using System.Threading.Tasks;
+    using Contracts;
+    using MassTransit;
+    using Microsoft.Extensions.Logging;
+
+    public class GettingStartedConsumer :
+        IConsumer<GettingStarted>
+    {
+        readonly ILogger<GettingStartedConsumer> _logger;
+
+        public GettingStartedConsumer(ILogger<GettingStartedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<GettingStarted> context)
+        {
+            _logger.LogInformation("Received Text: {Text}", context.Message.Value);
+            return Task.CompletedTask;
+        }
+    }
+}
